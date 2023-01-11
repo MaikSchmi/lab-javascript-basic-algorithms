78c259f347adb071c7bb46107e312c3b43af9d62
Maik Schmidt <maikschmidt91@gmail.com>
2023-01-11 11:43:14 +0100

    iteration-1 done

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
-
+console.log("Iteration-1");
+const hacker0 = "Maik";
+console.log(`The driver's name is ${hacker0}.`);
+const hacker1 = "Andy";
+console.log(`The navigator's name is ${hacker1}.`);
 
 // Iteration 2: Conditionals
 
