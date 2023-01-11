3b2510af532850f350f99c35fe14483196c36717
Maik Schmidt <maikschmidt91@gmail.com>
2023-01-11 11:46:25 +0100

    iteration-2 done

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const hacker1 = "Andy";
 console.log(`The navigator's name is ${hacker1}.`);
 
 // Iteration 2: Conditionals
-
+if (hacker0.length > hacker1.length) {
+    console.log(`The driver has the longest name, it has ${hacker0.length} characters.`);
+  } else if (hacker0.length < hacker1.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker1.length} characters.`);
+  } else {
+    console.log(`Wow, you both have equally long names, ${hacker0.length} characters!`);
+  }
 
 // Iteration 3: Loops
